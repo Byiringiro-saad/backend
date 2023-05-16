56086e0837db6b462ec0b168f17a9e00bc903bbc
Byiringiro Saad <byiringirosaad@gmail.com>
2023-05-16 23:35:14 +0200

    all controllers added

--- server.ts
+++ server.ts
@@ -2,8 +2,19 @@ import App from "./app";
 
 //controllers
 import userController from "./modules/user/user.controller";
+import cartController from "./modules/cart/cart.controller";
+import SeatController from "./modules/seat/seat.controller";
+import orderController from "./modules/order/order.controller";
+import productController from "./modules/product/product.controller";
 import RestaurantController from "./modules/restaurant/restaurant.controller";
 
-const app = new App([new userController(), new RestaurantController()]);
+const app = new App([
+  new userController(),
+  new cartController(),
+  new SeatController(),
+  new orderController(),
+  new productController(),
+  new RestaurantController(),
+]);
 
 app.listen();
